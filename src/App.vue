b0fc59b0c5016d31e16d05cd79da21d6d181e4dd
thepedroferrari <pedro@pedroferrari.com>
2021-05-02 21:57:22 +0200

    feat: create game board scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
   <h1>Vue Memory Game</h1>
+  <section class="game-board">
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+  </section>
 </template>
 
 <script>
@@ -16,5 +34,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.card {
+  border: 5px solid #ccc;
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: repeat(4, 100px);
+  grid-template-rows: repeat(4, 100px);
+  gap: 20px;
+  justify-content: center;
 }
 </style>
