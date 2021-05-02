125664c3cd4ff03dc34e468705129daadbe446c5
thepedroferrari <pedro@pedroferrari.com>
2021-05-02 23:28:59 +0200

    feat: Click function to card flip

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,26 @@
     <Card
       v-for="card in deck"
       :key="`${card.id}`"
-      :id="`${card.id}`"
-      :src="`${card.src}`"
+      :value="`${card.value}`"
+      :visible="card.visible"
+      :position="card.position"
+      @select-card="flipCard"
     />
   </section>
 </template>
 
 <script>
 import Card from "./components/Card";
-import genZ from "./assets/images/GenZ.jpg";
-import justCause3 from "./assets/images/JustCause3.jpg";
-import justCause4 from "./assets/images/JustCause4.jpg";
-import madMax from "./assets/images/MadMax.jpg";
-import rage2 from "./assets/images/Rage2.jpg";
-import secondExtinction from "./assets/images/SecondExtinction.jpg";
-import theHunter from "./assets/images/theHunter.jpg";
-import theHunter2 from "./assets/images/TheHunter2.jpg";
+// import genZ from "./assets/images/GenZ.jpg";
+// import justCause3 from "./assets/images/JustCause3.jpg";
+// import justCause4 from "./assets/images/JustCause4.jpg";
+// import madMax from "./assets/images/MadMax.jpg";
+// import rage2 from "./assets/images/Rage2.jpg";
+// import secondExtinction from "./assets/images/SecondExtinction.jpg";
+// import theHunter from "./assets/images/theHunter.jpg";
+// import theHunter2 from "./assets/images/TheHunter2.jpg";
+
+import { ref } from "vue";
 
 export default {
   name: "App",
@@ -28,76 +32,24 @@ export default {
   },
 
   setup() {
-    const deck = [
-      {
-        src: genZ,
-        id: "genZ-card1",
-      },
-      {
-        src: justCause3,
-        id: "justCause3-card1",
-      },
-      {
-        src: justCause4,
-        id: "justCause4-card1",
-      },
-      {
-        src: madMax,
-        id: "madMax-card1",
-      },
-      {
-        src: rage2,
-        id: "rage2-card1",
-      },
-      {
-        src: secondExtinction,
-        id: "secondExtinction-card1",
-      },
-      {
-        src: theHunter,
-        id: "theHunter-card1",
-      },
-      {
-        src: theHunter2,
-        id: "theHunter2-card1",
-      },
+    const deck = ref([]);
 
-      {
-        src: genZ,
-        id: "genZ-card2",
-      },
-      {
-        src: justCause3,
-        id: "justCause3-card2",
-      },
-      {
-        src: justCause4,
-        id: "justCause4-card2",
-      },
-      {
-        src: madMax,
-        id: "madMax-card2",
-      },
-      {
-        src: rage2,
-        id: "rage2-card2",
-      },
-      {
-        src: secondExtinction,
-        id: "secondExtinction-card2",
-      },
-      {
-        src: theHunter,
-        id: "theHunter-card2",
-      },
-      {
-        src: theHunter2,
-        id: "theHunter2-card2",
-      },
-    ];
+    for (let i = 0; i < 16; i += 1) {
+      deck.value.push({
+        id: i,
+        value: i,
+        position: i,
+        visible: false,
+      });
+    }
+
+    const flipCard = (payload) => {
+      deck.value[payload.position].visible = true;
+    };
 
     return {
       deck,
+      flipCard,
     };
   },
 };
@@ -111,10 +63,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.card {
-  border: 5px solid #ccc;
 }
 
 .game-board {
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,60 @@
-<template lang="">
-  <div class="card">
-    CARD
-  </div>
-</template>
 <script>
 export default {
-  
-}
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+    position: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  setup(props, context) {
+    const selectCard = () => {
+      context.emit("select-card", {
+        position: props.position,
+      });
+    };
+
+    return { selectCard };
+  },
+};
 </script>
-<style lang="">
-  
+
+
+<template>
+  <div class="card" @click="selectCard">
+    <div v-if="visible" class="card-face is-front">{{ value }}</div>
+    <div v-else class="card-face is-back">Back</div>
+  </div>
+</template>
+
+
+
+<style>
+.card {
+  border: 5px solid #ccc;
+  position: relative;
+}
+
+.card-face {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+
+.card-face.is-front {
+  background-color: red;
+  color: white;
+}
+.card-face.is-back {
+  background-color: blue;
+  color: white;
+}
 </style>
