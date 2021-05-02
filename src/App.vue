757d69feb035e6396160941981b021839a72d6de
thepedroferrari <pedro@pedroferrari.com>
2021-05-02 22:17:45 +0200

    chore: create deck of cards with images

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,106 @@
 <template>
   <h1>Vue Memory Game</h1>
   <section class="game-board">
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
+    <Card
+      v-for="card in deck"
+      :key="`${card.id}`"
+      :id="`${card.id}`"
+      :src="`${card.src}`"
+    />
   </section>
 </template>
 
 <script>
+import Card from "./components/Card";
+import genZ from "./assets/images/GenZ.jpg";
+import justCause3 from "./assets/images/JustCause3.jpg";
+import justCause4 from "./assets/images/JustCause4.jpg";
+import madMax from "./assets/images/MadMax.jpg";
+import rage2 from "./assets/images/Rage2.jpg";
+import secondExtinction from "./assets/images/SecondExtinction.jpg";
+import theHunter from "./assets/images/theHunter.jpg";
+import theHunter2 from "./assets/images/TheHunter2.jpg";
+
 export default {
-  name: 'App',
-}
+  name: "App",
+  components: {
+    Card,
+  },
+
+  setup() {
+    const deck = [
+      {
+        src: genZ,
+        id: "genZ-card1",
+      },
+      {
+        src: justCause3,
+        id: "justCause3-card1",
+      },
+      {
+        src: justCause4,
+        id: "justCause4-card1",
+      },
+      {
+        src: madMax,
+        id: "madMax-card1",
+      },
+      {
+        src: rage2,
+        id: "rage2-card1",
+      },
+      {
+        src: secondExtinction,
+        id: "secondExtinction-card1",
+      },
+      {
+        src: theHunter,
+        id: "theHunter-card1",
+      },
+      {
+        src: theHunter2,
+        id: "theHunter2-card1",
+      },
+
+      {
+        src: genZ,
+        id: "genZ-card2",
+      },
+      {
+        src: justCause3,
+        id: "justCause3-card2",
+      },
+      {
+        src: justCause4,
+        id: "justCause4-card2",
+      },
+      {
+        src: madMax,
+        id: "madMax-card2",
+      },
+      {
+        src: rage2,
+        id: "rage2-card2",
+      },
+      {
+        src: secondExtinction,
+        id: "secondExtinction-card2",
+      },
+      {
+        src: theHunter,
+        id: "theHunter-card2",
+      },
+      {
+        src: theHunter2,
+        id: "theHunter2-card2",
+      },
+    ];
+
+    return {
+      deck,
+    };
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Card.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div class="card">
+    CARD
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="">
+  
+</style>
