4e38f8b3b1154b63a73e5656d187a038e1a0d97c
thepedroferrari <pedro@pedroferrari.com>
2021-05-08 14:00:24 +0200

    feat: style leaderboard fixed

--- src/components/Leaderboard/Leaderboard.vue
+++ src/components/Leaderboard/Leaderboard.vue
@@ -1,35 +1,37 @@
 <template>
-  <h2 class="title">Leaderboard</h2>
-  <ul class="leaderboard-results">
-    <li>
-      <span class="name">
-        <strong>Name</strong>
-      </span>
-      <span class="device">
-        <strong>Device</strong>
-      </span>
-      <span class="clicks">
-        <strong>Clicks</strong>
-      </span>
-      <span class="score">
-        <strong>Time</strong>
-      </span>
-    </li>
-    <li
-      v-for="(entry, index) in leaderboard"
-      :key="entry.id"
-      :name="entry.name"
-      :device="entry.device"
-      :clicks="entry.clicks"
-    >
-      <span class="name"
-        ><span class="position">{{ index + 1 }}.</span> {{ entry.name }}</span
+  <section class="leaderboard-modal">
+    <h3 class="title">Leaderboard</h3>
+    <ul class="leaderboard-results">
+      <li>
+        <span class="name">
+          <strong>Name</strong>
+        </span>
+        <span class="device">
+          <strong>Device</strong>
+        </span>
+        <span class="clicks">
+          <strong>Clicks</strong>
+        </span>
+        <span class="score">
+          <strong>Time</strong>
+        </span>
+      </li>
+      <li
+        v-for="(entry, index) in leaderboard"
+        :key="entry.id"
+        :name="entry.name"
+        :device="entry.device"
+        :clicks="entry.clicks"
       >
-      <span class="device">{{ entry.device }}</span>
-      <span class="clicks">{{ entry.clicks }}</span>
-      <span class="score">{{ entry.score }}s</span>
-    </li>
-  </ul>
+        <span class="name"
+          ><span class="position">{{ index + 1 }}.</span> {{ entry.name }}</span
+        >
+        <span class="device">{{ entry.device }}</span>
+        <span class="clicks">{{ entry.clicks }}</span>
+        <span class="score">{{ entry.score }}s</span>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
